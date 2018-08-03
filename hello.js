83425669626bc77bb105ebf1603948075d719932
Mai Truong Giang <giangrbr@gmail.com>
2018-08-03 12:56:35 +0700

    Add JS, hello world

---
+++ hello.js
@@ -0,0 +1,8 @@
+let firstName = "Giang";
+let lastName = "Mai";
+let username = firstName.toLowerCase();
+
+console.log("Hello, world!");
+console.log(`My name is ${firstName}.`);
+console.log(`Full name: ${lastName} ${firstName}.`);
+console.log(`Email: ${username}@example.com`);
